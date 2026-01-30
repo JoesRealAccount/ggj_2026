95c5ace79d779264c5e6ca7f2e10f6286995ca8f
Joe Blub <dont@write-me>
2026-01-30 23:07:12 +0100

    makingg the mask reset to happy on player death

--- mask_manager.gd
+++ mask_manager.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var shader_overlay_manager: ShaderOverlayManager
 
+func _ready() -> void:
+	SignalManager.player_death.connect(_on_player_death)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("switch_mask"):
 		match shader_overlay_manager.current_overlay:
@@ -9,3 +12,6 @@ func _input(event: InputEvent) -> void:
 				shader_overlay_manager.current_overlay = shader_overlay_manager.overlay_types.DEPRESSION
 			_:
 				shader_overlay_manager.current_overlay = shader_overlay_manager.overlay_types.HAPPYNESS
+
+func _on_player_death():
+	shader_overlay_manager.current_overlay = shader_overlay_manager.overlay_types.HAPPYNESS
