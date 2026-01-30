75159fc9e48c3a284f98f55a0c690df8c850cc83
Shinigami92 <chrissi92@hotmail.de>
2026-01-30 22:06:18 +0100

    restructure folders

--- scenes/main_level.gd
+++ scenes/main_level.gd
@@ -3,4 +3,4 @@ extends Node3D
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("back_to_menu"):
-		get_tree().change_scene_to_file("res://main_screen.tscn")
+		get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
