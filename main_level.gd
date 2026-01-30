4bb337394555609b78a7054c177d0a199bffd64c
Shinigami92 <chrissi92@hotmail.de>
2026-01-30 21:03:31 +0100

    Refactor main level scene: update player transform positions and improve process function parameter naming

--- main_level.gd
+++ main_level.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("back_to_menu"):
 		get_tree().change_scene_to_file("res://main_screen.tscn")
