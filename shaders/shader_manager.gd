b7ca1913da374643f39d4c19f15354e04e43e047
Joe Blub <dont@write-me>
2026-01-30 20:17:11 +0100

    working on the shader

--- shaders/shader_manager.gd
+++ shaders/shader_manager.gd
@@ -4,21 +4,14 @@ extends ColorRect
 
 @export_group("Overlay Color")
 
-@export var overlay_color: Color = Color.WHITE:
-	set(new_value):
-		new_value.a = overlay_color.a
-		return new_value
 
-@export var opacity: float = 1.0:
-	set(new_value):
-		overlay_color.a = new_value
-		return new_value
-
+@export var normal_color: Color = Color(0.0, 0.0, 1.0, 0.0)
+@export var overlay_color: Color = Color.BLACK
 
 
 func _ready() -> void:
-	_screen_overlay_rect.color = overlay_color
-	# tween_to_newscale(3.0)
+	_screen_overlay_rect.color = normal_color
+	tween_to_newscale(3.0)
 
 func tween_to_newscale(tween_time_sec: float):
 	get_tree().create_tween().tween_property(_screen_overlay_rect, "color", overlay_color, tween_time_sec)
