ecbe11d9af66cf82d7f7b06205ea4caa4993e302
Shinigami92 <chrissi92@hotmail.de>
2026-01-30 19:57:51 +0100

    Add main menu scene with start and quit buttons; update main scene reference

--- main_level.gd
+++ main_level.gd
@@ -1,13 +1,6 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Handle exit game
-	if Input.is_action_just_pressed("exit_game"):
-		get_tree().quit()
+	if Input.is_action_just_pressed("back_to_menu"):
+		get_tree().change_scene_to_file("res://main_screen.tscn")
