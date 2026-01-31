b73a62d9fcc658c03f0ec56eee4e39cd730a705e
Joe Blub <dont@write-me>
2026-01-31 13:20:06 +0100

    still working on audioeffects

---
+++ music/audio_effect_collection.gd
@@ -0,0 +1,4 @@
+class_name AudioEffectCollection
+extends Resource
+
+@export var _audio_effects: Array[AudioEffect]
--- prefabs/music_manager.gd
+++ prefabs/music_manager.gd
@@ -1,11 +1,12 @@
 extends AudioStreamPlayer
 
 
+@onready var _music_bus_index: int = AudioServer.get_bus_index("Music")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass # Replace with function body
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_mask_toggled(is_depression: bool):
 	pass
+
