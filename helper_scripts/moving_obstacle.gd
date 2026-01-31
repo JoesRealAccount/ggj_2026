8848537ee2f1ffc5bff3c70cf1eb2d50b9af38b4
Joe Blub <dont@write-me>
2026-01-31 15:54:17 +0100

    adding random offset for platform speed

--- helper_scripts/moving_obstacle.gd
+++ helper_scripts/moving_obstacle.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
-@export var speed: float = 5.0
+@export var min_speed: float = 3.0
+@export var max_speed: float = 5.0
+@onready var _speed: float = randf_range(3.0, 5.0)
 
 func _physics_process(delta: float) -> void:
-	position.x -= speed * delta
+	position.x -= _speed * delta
 
 
