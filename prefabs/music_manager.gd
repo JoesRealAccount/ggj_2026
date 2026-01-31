cf0d011377e9c0ec2d700c661b5e0288651e898a
Joe Blub <dont@write-me>
2026-01-31 14:10:19 +0100

    actually fixing said index out of bounds

--- prefabs/music_manager.gd
+++ prefabs/music_manager.gd
@@ -10,8 +10,8 @@ func _ready() -> void:
 	SignalManager.world_toggled_is_depression.connect(_on_mask_toggled)
 
 func _on_mask_toggled(is_depression: bool):
-	for effect_index: int in range(AudioServer.get_bus_effect_count(_music_bus_index)-1):
-		AudioServer.remove_bus_effect(_music_bus_index, effect_index)
+	for effect_index: int in range(AudioServer.get_bus_effect_count(_music_bus_index)):
+		AudioServer.remove_bus_effect(_music_bus_index, 0)
 	if is_depression:
 		for _audio_effect: AudioEffect in _audio_effect_collection._audio_effects:
 			AudioServer.add_bus_effect(_music_bus_index, _audio_effect, AudioServer.get_bus_effect_count(_music_bus_index))
