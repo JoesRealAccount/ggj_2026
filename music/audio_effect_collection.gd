b5e0b7f8b6dc52ef1b8bfde160fd1188688c7d0c
Joe Blub <dont@write-me>
2026-01-31 14:17:58 +0100

    refactoring private -> publoic variable

--- music/audio_effect_collection.gd
+++ music/audio_effect_collection.gd
@@ -1,5 +1,4 @@
 class_name AudioEffectCollection
 extends Resource
 
-@warning_ignore("unused_parameter")
-@export var _audio_effects: Array[AudioEffect]
+@export var audio_effects: Array[AudioEffect]
