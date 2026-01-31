a26a9b7add1729f7cfd58a0f272206d0ba80d7ae
Joe Blub <dont@write-me>
2026-01-31 13:57:43 +0100

    making the mask toggle also toggle music

--- prefabs/music_manager.gd
+++ prefabs/music_manager.gd
@@ -10,9 +10,11 @@ func _ready() -> void:
 	SignalManager.world_toggled_is_depression.connect(_on_mask_toggled)
 
 func _on_mask_toggled(is_depression: bool):
-	for effect_index: int in AudioServer.get_bus_effect_count(_music_bus_index)-1:
+	for effect_index: int in range(AudioServer.get_bus_effect_count(_music_bus_index)):
 		AudioServer.remove_bus_effect(_music_bus_index, effect_index)
 	if is_depression:
 		for _audio_effect: AudioEffect in _audio_effect_collection._audio_effects:
 			AudioServer.add_bus_effect(_music_bus_index, _audio_effect, AudioServer.get_bus_effect_count(_music_bus_index))
-	print(AudioServer.get_bus_effect_count(_music_bus_index))
+		AudioServer.set_bus_bypass_effects(_music_bus_index, false)
+	else:
+		AudioServer.set_bus_bypass_effects(_music_bus_index, true)
