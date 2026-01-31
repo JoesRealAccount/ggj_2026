91143c61b6c8e8183f20b56fa896c6191a1d0284
Shinigami92 <chrissi92@hotmail.de>
2026-01-31 12:15:12 +0100

    Add new chair prefab and integrate into main level scene

--- prefabs/throw_spawning_system.gd
+++ prefabs/throw_spawning_system.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var throwable_objects: Array[PackedScene] = []
 @export var spawn_interval: float = 2.0
 @export var object_speed: float = 10.0
+@export var object_rotation_speed: float = 2.0
 @export var spawn_x: float = 17.0
 @export var spawn_y_min: float = 0.8
 @export var spawn_y_max: float = 15.0
@@ -41,7 +42,12 @@ func spawn_random_object() -> void:
 		if instance is RigidBody3D:
 			instance.linear_velocity = Vector3(-object_speed, 0, 0)
 	
-	# Alternative: use a tween to animate position from right to left
-	var tween = create_tween()
-	tween.tween_property(instance, "position:x", -17.0, 40.0 / object_speed)
-	tween.tween_callback(instance.queue_free)
+	# Tween for horizontal movement
+	var movement_tween = create_tween()
+	movement_tween.tween_property(instance, "position:x", -17.0, 40.0 / object_speed)
+	movement_tween.tween_callback(instance.queue_free)
+	
+	# Separate tween for continuous rotation
+	var rotation_tween = create_tween()
+	rotation_tween.set_loops()
+	rotation_tween.tween_property(instance, "rotation:z", instance.rotation.z + TAU, object_rotation_speed).from(instance.rotation.z)
