2480e34cf8a95e1259e76990a5c0f35a919936b1
Joe Blub <dont@write-me>
2026-02-01 12:00:46 +0100

    fixing kill system

--- prefabs/kill_area.gd
+++ prefabs/kill_area.gd
@@ -1,5 +1,8 @@
-extends Node3D
+extends Area3D
 
+
+func _ready() -> void:
+	body_entered.connect(_on_area_3d_body_entered)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
